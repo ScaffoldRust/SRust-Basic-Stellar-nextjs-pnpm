70decb002eab247b030f3c09a61fc99b50a4577f
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-28 13:53:02 -0600

    feat: set Freighter as the default wallet in wallet constants

--- packages/nextjs/src/constants/wallet.constants.ts
+++ packages/nextjs/src/constants/wallet.constants.ts
@@ -2,9 +2,11 @@ import {
     StellarWalletsKit,
     WalletNetwork,
     allowAllModules,
+    FREIGHTER_ID, 
   } from "@creit.tech/stellar-wallets-kit";
   
   export const kit: StellarWalletsKit = new StellarWalletsKit({
     network: WalletNetwork.TESTNET,
+    selectedWalletId: FREIGHTER_ID, // set Freighter as the default wallet
     modules: allowAllModules(),
   });
